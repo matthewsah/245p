699f1625c27316596971688d3c56ccac8496e42b
Matthew Sah <mattsah2001@gmail.com>
2023-11-08 19:59:37 -0800

    exercise 7

---
+++ exercise7/client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import Header from './Header';
+import Sidebar from './Sidebar'
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Sidebar />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ exercise7/client/src/Sidebar.js
@@ -0,0 +1,12 @@
+import './Sidebar.css'
+
+function Sidebar() {
+  return (<div className="sidebar">
+    <a href="#about">About</a>
+    <a href="#services">Services</a>
+    <a href="#clients">Clients</a>
+    <a href="#contact">Contact</a>
+  </div>);
+}
+
+export default Sidebar;
